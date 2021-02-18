fbd20f62a646c9162871e22b2c98de84f79990ee
abudzr <abudzaralghifari8@gmail.com>
2021-02-18 07:47:16 +0700

    update sorting

--- no3.js
+++ no3.js
@@ -1,9 +1,8 @@
 const seleksiNilai = (nilaiAwal,nilaiAkhir,dataArray)=>{
     if (typeof(nilaiAwal) === 'number' && typeof(nilaiAkhir) === 'number' && typeof(dataArray) === 'object' ) {
-        const cari = dataArray.filter((x)=> {
-            return x < nilaiAkhir;
-            // nilai awal belum masuk
-            // hasil belum disortir
+        const cari = dataArray.filter((x)=> {      
+            dataArray.sort(function(a, b){return a-b});
+            return x > nilaiAwal && x < nilaiAkhir;
         })
         if (nilaiAwal>nilaiAkhir) {
             console.log('Nilai akhir harus lebih besar dari nilai awal');
@@ -16,5 +15,4 @@ const seleksiNilai = (nilaiAwal,nilaiAkhir,dataArray)=>{
         console.log("Masukkan Data yang benar");
     }
 }
-seleksiNilai(1,17,[1,2,4,6,9,10,15,19,20,90,54,3,4,5,6,7,8,9,10,13,16,17])
-
+seleksiNilai(4,20,[2,4,6,9,10,15,19,20,90,54,3,4,5,6,7,8,9,10,13,17])
